4989010982ff542eb4f8850f67dc72483c12f265
scruffyscarf <114242238+scruffyscarf@users.noreply.github.com>
2024-07-03 23:39:39 +0300

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <a href="comic.html">XKCD-Comic</a>
+        </footer>
+    );
+};
+
+export default Footer;
